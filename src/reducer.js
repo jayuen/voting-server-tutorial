c092eeaea66bb33ee6dd7db076733a81968e8531
Jason Yuen <jasony@nulogy.com>
2015-12-09 00:59:25 -0500

    moves initial state to core

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,5 @@
-import { Map } from 'immutable';
-import { setEntries, vote, next } from './core'; 
-export default function reducer(state = Map(), action) {
+import { setEntries, vote, next, INITIAL_STATE } from './core'; 
+export default function reducer(state = INITIAL_STATE, action) {
   switch (action.type) {
     case 'SET_ENTRIES':
       return setEntries(state, action.entries);
